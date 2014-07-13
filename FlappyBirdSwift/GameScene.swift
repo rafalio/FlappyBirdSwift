5a98e410bd0686caa6c9e86b18ed363bf9fafb12
Rafal Szymanski <me@rafal.io>
2014-07-13 23:41:36 +0200

    basic functionality

--- FlappyBirdSwift/GameScene.swift
+++ FlappyBirdSwift/GameScene.swift
@@ -27,21 +27,28 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var score:Int = 0
     var gameOverLabel:SKLabelNode!
     var scoreLabel:SKLabelNode!
+    var pipeAccum:Array<SKNode> = []
     
     
     var appState:GameState = .Waiting{
         didSet{
             if(appState == .Waiting){
-                resetPlayerPosition()
+                gameOverLabel.removeFromParent()
+                scoreLabel.removeFromParent()
+                resetLevel()
                 self.paused = true
             }
             else if(appState == .Playing){
                 self.paused = false
+                spawnPipes()
             }
             else if(appState == .DeathScreen){
                 // nothing
+                self.addChild(gameOverLabel)
+                scoreLabel.text = "\(score)"
+                self.addChild(scoreLabel)
+                self.paused = true
             }
-            updateUI()
         }
     }
     
@@ -49,28 +56,29 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     override func didMoveToView(view: SKView) {
         println("Width: \(view.frame.size.width), height: \(view.frame.size.height)")
         self.backgroundColor = UIColor.whiteColor()
-        self.anchorPoint = CGPointMake(0.5,0.5);
         self.paused = true // pause the scene
         self.physicsWorld.contactDelegate = self
         
-        let pSize:CGFloat = 30
+        let pSize:CGFloat = 20
         player = SKSpriteNode(color:UIColor.blackColor(), size:CGSizeMake(pSize, pSize))
-        resetPlayerPosition()
+        resetLevel()
         
         player.physicsBody = SKPhysicsBody(rectangleOfSize:CGSizeMake(pSize,pSize));
         player.physicsBody.categoryBitMask    = playerCategory
         player.physicsBody.contactTestBitMask = groundCategory
         player.physicsBody.collisionBitMask   = groundCategory
+        player.physicsBody.mass = 2
+        player.physicsBody.allowsRotation = false
+        
         self.addChild(player)
         
         // The base that doesn't take part in the physics simulation
         let base = SKSpriteNode(color:UIColor.brownColor(), size: CGSizeMake(view.frame.size.width, 100))
-        base.position = CGPointMake(0,-CGRectGetMidY(view.frame)+base.size.height/2)
+        base.position = CGPointMake(view.frame.width/2,base.size.height/2)
         self.addChild(base)
         
-        
         let ground = SKSpriteNode(color:UIColor.blueColor(), size: CGSizeMake(view.frame.size.width, 10))
-        ground.position = CGPointMake(0,base.position.y + base.size.height/2)
+        ground.position = CGPointMake(view.frame.width/2, base.position.y + base.size.height/2 + ground.size.height/2)
         
         ground.physicsBody = SKPhysicsBody(rectangleOfSize:ground.size)
         ground.physicsBody.categoryBitMask = groundCategory
@@ -81,10 +89,32 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         setUpLabels()
         
-        var p = getPipe()
-        
-        
-        self.addChild(p)
+    }
+    
+    func spawnPipes(){
+        if(appState == .Playing){
+            let pipes = getPipe()
+            pipes.position = CGPoint(x: view.frame.width + 30, y: pipes.position.y)
+            self.addChild(pipes)
+            pipeAccum.append(pipes)
+            
+            let velocity:Double = 130 // pixels per second
+            let dist:Double     = Double(view.frame.size.width)
+            let time            = dist/velocity
+            let sucDist:Double  = 130 // distance between succesive pipes
+            let waitTime:Double = sucDist/velocity
+            
+            let animAction = SKAction.moveBy(CGVectorMake(-(view.frame.width+100), 0), duration: time)
+            let doneAction = SKAction.sequence([SKAction.removeFromParent(), SKAction.runBlock({() in
+                self.pipeAccum.removeAtIndex(0) // inefficient, ideally this should be a queue, but... well swift doesn't have one
+                return ()
+                })])
+            let recurse    = SKAction.runBlock({
+                self.spawnPipes()
+            })
+            let compoundAction = SKAction.sequence([SKAction.waitForDuration(waitTime), recurse, animAction, doneAction])
+            pipes.runAction(compoundAction)
+        }
         
     }
     
@@ -102,42 +132,35 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     func didBeginContact(contact: SKPhysicsContact!){
-        if(appState != .DeathScreen){
+        if(appState != .DeathScreen){ // any kind of touch makes the player die
             appState = .DeathScreen
         }
     }
     
     func thrustBird(){
         player.physicsBody.velocity = CGVectorMake(0, 0)
-        player.physicsBody.applyImpulse(CGVectorMake(0, 19))
+        player.physicsBody.applyImpulse(CGVectorMake(0, 800))
     }
     
-    func updateUI(){
-        if(appState == .DeathScreen){
-            self.addChild(gameOverLabel)
-            scoreLabel.text = "\(score)"
-            self.addChild(scoreLabel)
-        }
-        else{
-            gameOverLabel.removeFromParent()
-            scoreLabel.removeFromParent()
-        }
-    }
     
-    func resetPlayerPosition(){
-        player.position = CGPointMake(-60,0)
+    func resetLevel(){
+        player.position = CGPointMake(view.frame.width/2 - 50, view.frame.height/2)
+        for p in pipeAccum{
+            p.removeFromParent()
+        }
+        pipeAccum.removeAll(keepCapacity: true)
     }
     
     func setUpLabels(){
         gameOverLabel = SKLabelNode(fontNamed:"Avenir")
         gameOverLabel.text = "Game Over"
         gameOverLabel.fontSize = 25
-        gameOverLabel.position = CGPointMake(0,0)
+        gameOverLabel.position = CGPointMake(view.frame.width/2,view.frame.height/2)
         gameOverLabel.fontColor = UIColor.blackColor()
         
         scoreLabel = SKLabelNode(fontNamed:"Avenir")
         scoreLabel.fontSize = 25
-        scoreLabel.position = CGPointMake(0,-30)
+        scoreLabel.position = CGPointMake(view.frame.width/2,view.frame.height/2-30)
         scoreLabel.fontColor = UIColor.blackColor()
     }
     
@@ -147,6 +170,15 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     // returns an SKNode with the top and bottom pipe
     func getPipe() -> SKNode{
+        
+        func addPipePhysics(pipe: SKSpriteNode){
+            pipe.physicsBody = SKPhysicsBody(rectangleOfSize:pipe.size)
+            pipe.physicsBody.categoryBitMask = pipeCategory
+            pipe.physicsBody.contactTestBitMask = playerCategory
+            pipe.physicsBody.collisionBitMask = 0
+            pipe.physicsBody.affectedByGravity = false
+        }
+        
         let holeStartY = CGFloat((arc4random() % 200) + 200)
         let pipeStartY:CGFloat = 110
         
@@ -154,16 +186,17 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         let topPieceHeight:CGFloat = 500 // make the piece on top extend beyond the top of the screen for the bird to crash
         
         let bottomPiece = SKSpriteNode(color: UIColor.blackColor(), size: CGSizeMake(pipeWidth, bottomPieceHeight))
-        bottomPiece.position = CGPointMake(0, 0)
+        bottomPiece.position = CGPointMake(pipeWidth/2, bottomPieceHeight/2 + 110)
         bottomPiece.color = UIColor.redColor()
+        addPipePhysics(bottomPiece)
         
         let topPiece = SKSpriteNode(color: UIColor.blackColor(), size: CGSizeMake(pipeWidth, topPieceHeight))
-        topPiece.position = CGPointMake(0,topPiece.size.height/2 + pipeVSpacing + bottomPieceHeight/2)
+        topPiece.position = CGPointMake(pipeWidth/2, bottomPiece.position.y + bottomPiece.size.height/2 + pipeVSpacing + topPieceHeight/2)
+        addPipePhysics(topPiece)
         
         let node = SKNode()
         node.addChild(topPiece)
         node.addChild(bottomPiece)
-        
         
         return node
     }
